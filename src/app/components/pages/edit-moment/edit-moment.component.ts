28e5ee0b779fbe681ea8979e6d4e37ab112538d6
natanaelrocha <natanaelmoreirarocha1@gmail.com>
2023-08-22 21:33:53 -0300

    first commit

---
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+import { Moment } from 'src/app/Moments';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { ActivatedRoute, Route, Router } from '@angular/router';
+
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-edit-moment',
+  templateUrl: './edit-moment.component.html',
+  styleUrls: ['./edit-moment.component.css']
+})
+export class EditMomentComponent {
+  moment!:Moment;
+
+  btnText:string = "Editar"
+  constructor(
+    private momentService:MomentService, 
+    private route: ActivatedRoute, 
+    private messagensService: MessagesService,
+    private router: Router
+    ){}
+
+  
+  ngOnInit():void{
+    const id =Number(this.route.snapshot.paramMap.get("id"))
+
+    this.momentService.getMoment(id).subscribe((item) => {
+      this.moment = item.data;
+    })
+  }
+
+  async editHandler(momentData: Moment){
+    const id = this.moment.id
+
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+
+    if(momentData.image){
+      formData.append('description', momentData.description)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messagensService.add(`Moment ${id} foi atualizado com sucesso!`)
+
+    this.router.navigate(['/'])
+  }
+}
---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { Moment } from 'src/app/Moments';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { MessagesService } from 'src/app/services/messages.service';
+
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+  btnText = "Compartilhar!"
+
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService,
+    private router:Router
+  ){}
+  async createdHandler(moment: Moment){
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image){
+      formData.append("image", moment.image)
+    }
+
+    await this.momentService.createdMoment(formData).subscribe();
+
+    //exibir menssagem 
+    this.messagesService.add("Momento adicionado com sucesso!")
+
+    //redirect
+
+    this.router.navigate(["/"])
+  }
+}
